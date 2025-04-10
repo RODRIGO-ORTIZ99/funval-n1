cf3c44a3942cd55a932e9e4b3537bfa6aee79ba6
Rodri <hello@192.168.1.8>
2025-04-10 14:59:47 -0600

    practica

---
+++ dark-mode/dark.js
@@ -0,0 +1,8 @@
+let buttonDark = document.querySelector("#dark");
+
+buttonDark.addEventListener("click", switchDarkMode)
+
+function switchDarkMode() {
+    document.documentElement.classList.toggle("dark");
+    console.log()
+}
